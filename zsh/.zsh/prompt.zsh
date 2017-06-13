3e88031235831ef24080d65a68b0cb184319c131
jonathan <wiggins.jonathan@gmail.com>
2017-06-13 07:47:21 -0700

    Set up git info in zsh prompt

--- zsh/.zsh/prompt.zsh
+++ zsh/.zsh/prompt.zsh
@@ -4,5 +4,16 @@ hostname=%m
 pwd=%~
 newline=$'\n'
 prompt=%#
+#git=$(__git_ps1 "%s")
 
-PROMPT="┌──[%F{magenta}${username}%f@%F{magenta}${hostname}%f] [%F{blue}%B${pwd}%b%f]${newline}└─${prompt} "
+# %F/%f = start/stop text color
+# %B/%b = start/stop bold color
+
+setopt PROMPT_SUBST
+
+PROMPT="%B┌──[%F{magenta}${username}%f@%F{magenta}${hostname}%f] \
+[%F{blue}${pwd}%f]${newline}\
+└─${prompt}%b "
+
+RPROMPT='%B%F{red}$(__git_ps1 "[%s]")%f%b'
+#RPROMPT=${git}
