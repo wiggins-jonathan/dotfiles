7f2a0a0257fe8a72e6b3c5704f4c26a5d148fcb3
jonathan <wiggins.jonathan@gmail.com>
2017-06-15 13:48:27 -0700

    Change method for git prompt info from git's __git_ps1 function to zsh's more extensive vcs_info function

--- zsh/.zsh/prompt.zsh
+++ zsh/.zsh/prompt.zsh
@@ -1,19 +1,38 @@
 # Set variables for easier prompt expansion
+# See SIMPLE PROMPT ESCAPES in the zshmisc man page
 username=%n
 hostname=%m
 pwd=%~
 newline=$'\n'
 prompt=%#
-#git=$(__git_ps1 "%s")
 
 # %F/%f = start/stop text color
 # %B/%b = start/stop bold color
 
+# Enable vcs_info function
+autoload -Uz vcs_info
+
+# Enable vcs_info for git. Other vcs (svn, hr, csv, etc) can be enabled here
+zstyle ':vcs_info:*' enable git
+
+# Configure vcs_info in prompt 
+# See GATHERING INFORMATION FROM VERSION CONTROL SYSTEMS in the zshcontrib man page
+zstyle ':vcs_info:*' check-for-changes true
+zstyle ':vcs_info:*' stagedstr '[%F{red}↑%f]'
+zstyle ':vcs_info:*' unstagedstr '[%F{red}⚡%f]'
+zstyle ':vcs_info:*' formats '[%F{red}%s-%b%f] %c%u'
+zstyle ':vcs_info:*' actionformats '[%F{magenta}%s-%b%f] %c%u'
+
+# Update vcs_info when prompt is printed to screen
+precmd() {
+	vcs_info
+}
+
+# Set parameter expansion, command substitution, & arithmetic expansion in prompt
 setopt PROMPT_SUBST
 
 PROMPT="%B┌──[%F{magenta}${username}%f@%F{magenta}${hostname}%f] \
 [%F{blue}${pwd}%f]${newline}\
 └─${prompt}%b "
 
-RPROMPT='%B%F{red}$(__git_ps1 "[%s]")%f%b'
-#RPROMPT=${git}
+RPROMPT='%B${vcs_info_msg_0_}%b'
