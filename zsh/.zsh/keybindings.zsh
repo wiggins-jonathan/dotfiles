6bb1d1bfc0b32a83a5661aa1d555d9022fc9ba42
Jonathan Wiggins <wiggins.jonathan@gmail.com>
2017-08-30 03:28:21 -0700

    Add escape sequences for home, end, & delete to keybindings.

--- zsh/.zsh/keybindings.zsh
+++ zsh/.zsh/keybindings.zsh
@@ -1,3 +1,6 @@
-#Set Ctrl+left & Ctrl+right arrows to go move cursor back & forward a word, respectively.
+# Map escape sequences to keys using bindkey. Get the escape sequences using the showkey -a command. 
 bindkey "^[[1;5C" forward-word
 bindkey "^[[1;5D" backward-word
+bindkey "^[[H" beginning-of-line
+bindkey "^[[F" end-of-line
+bindkey "^[[3~" delete-char
