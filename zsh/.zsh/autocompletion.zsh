93ec00516fc411ff611f24ebd419385870a381d6
Jonathan Wiggins <wiggins.jonathan@gmail.com>
2017-08-30 10:28:50 -0700

    zsh-syntax-highlighting for less seems to be broken in the newest version. Will display error message & will not color less output.

--- zsh/.zsh/autocompletion.zsh
+++ zsh/.zsh/autocompletion.zsh
@@ -9,10 +9,9 @@ zstyle ':completion:*:default' list-colors ${(s.:.)LS_COLORS}
 # Colors for the kill command
 zstyle ':completion:*:*:kill:*:processes' list-colors '=(#b) #([0-9]#)*=0=01;31'	
 
+# This seems to be broken in the zsh-syntax-highlighting 0.6.0. Will give error message after using less.
 # Set color for the less command using the source-highlight program
-export LESSOPEN="| /usr/bin/source-highlight-esc.sh %s"
-export LESS=' -R '
+#export LESSOPEN="| /usr/bin/source-highlight-esc.sh %s"
+#export LESS=' -R '
 
 zstyle ':completion:*' menu select	# Use menu select tab completion instead of autocomplete
-
-
