7b6d4a4f2b751e91e59908a37d4743751c311d24
Jonathan Wiggins <wiggins.jonathan@gmail.com>
2017-08-30 03:54:46 -0700

    Change around the format of history.zsh & fixed typo

--- zsh/.zsh/history.zsh
+++ zsh/.zsh/history.zsh
@@ -1,12 +1,7 @@
-# set history size
-export HISTSIZE=1000
-# save history after logout
-export SAVEHIST=1000
-# save to history file
-export HISTFILE=~/.zsh/zhistory
-# append to this history file
-setopt INC_APPEND_HISTORY
-# add timestampt for each history entry
-setopt EXTENDED_HISTORY
-# avoid duplicate entries
-setopt HIST_IGNORE_DUPS
+# History file for zsh. See History entry in man zshoptions for options
+export HISTSIZE=1000			# set history size
+export SAVEHIST=1000			# save history after logout
+export HISTFILE=~/.zsh/zhistory		# save to history file
+setopt INC_APPEND_HISTORY		# append to this history file
+setopt EXTENDED_HISTORY			# add timestamp for each history entry
+setopt HIST_IGNORE_DUPS			# avoid duplicate entries
