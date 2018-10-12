03d9297c9a92476bdef7220b470b30eee20b282a
Jonathan Wiggins <wiggins.jonathan@gmail.com>
2018-10-11 18:04:33 -0700

    Add a function that ls whenever cd

--- zsh/.zsh/aliases.zsh
+++ zsh/.zsh/aliases.zsh
@@ -1,9 +1,19 @@
-#===================#
-#       Aliases     #
-#===================# 
+#=======================================#
+#            ___                        #
+#     ____ _/ (_)___ _________  _____   #
+#    / __ `/ / / __ `/ ___/ _ \/ ___/   #
+#   / /_/ / / / /_/ (__  )  __(__  )    #
+#   \__,_/_/_/\__,_/____/\___/____/     #
+#                                       #
+#=======================================#
 
 alias ls='ls -lAhN --color=auto --group-directories-first'
 alias grep='grep --color=auto'
 alias diff='diff --color=auto'
 alias ip='ip -c'
 alias ssh="TERM=termite ssh"
+
+# cd & ls together forever
+cdl() {
+    builtin cd "$@" && ls;
+}
