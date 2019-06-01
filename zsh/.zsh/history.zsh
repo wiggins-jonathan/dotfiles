989ff21c0f61f61ae78314af1dd6234338f1bd7c
Jonathan Wiggins <wiggins.jonathan@gmail.com>
2019-05-31 17:08:23 -0700

    Fix some whitespace issues for easier readability

--- zsh/.zsh/history.zsh
+++ zsh/.zsh/history.zsh
@@ -1,7 +1,7 @@
 # History file for zsh. See History entry in man zshoptions for options
-export HISTSIZE=1000			    # set history size
-export SAVEHIST=1000			    # save history after logout
-export HISTFILE=~/.zsh/zhistory		# save to history file
-setopt INC_APPEND_HISTORY		    # append to this history file
-setopt EXTENDED_HISTORY			    # add timestamp for each history entry
-setopt HIST_IGNORE_DUPS			    # avoid duplicate entries
+export HISTSIZE=1000            # set history size
+export SAVEHIST=1000            # save history after logout
+export HISTFILE=~/.zsh/zhistory # save to history file
+setopt INC_APPEND_HISTORY       # append to this history file
+setopt EXTENDED_HISTORY         # add timestamp for each history entry
+setopt HIST_IGNORE_DUPS         # avoid duplicate entries
--- zsh/.zsh/prompt.zsh
+++ zsh/.zsh/prompt.zsh
@@ -5,9 +5,9 @@ pwd=%~
 newline=$'\n'
 git='${vcs_info_msg_0_}'
 
-autoload -Uz colors && colors       # Load & call colors function
-autoload -Uz vcs_info			    # Enable vcs_info function
-zstyle ':vcs_info:*' enable git		# Enable vcs info for git specifically
+autoload -Uz colors && colors   # Load & call colors function
+autoload -Uz vcs_info           # Enable vcs_info function
+zstyle ':vcs_info:*' enable git # Enable vcs info for git specifically
 
 # %F/%f = start/stop text color
 # %B/%b = start/stop bold color
@@ -21,10 +21,10 @@ zstyle ':vcs_info:*' actionformats '[%F{magenta}%s-%b%f] %c%u'
 
 # Update vcs_info when prompt is printed to screen
 precmd() {
-	vcs_info
+  vcs_info
 }
 
-setopt PROMPT_SUBST	# Set parameter expansion, command substitution, & arithmetic expansion in prompt
+setopt PROMPT_SUBST # Set parameter expansion, command substitution, & arithmetic expansion in prompt
 
 PROMPT="%B┌──[%F{magenta}${username}%f@%F{magenta}${hostname}%f] [%F{blue}${pwd}%f] ${git}${newline}\
 └─%F{blue}%f%b " 
