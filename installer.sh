fd41953918ff49b66f3978b9448375e2db1bcb36
Jonathan Wiggins <wiggins.jonathan@gmail.com>
2020-10-21 15:04:43 -0700

    Whitespace fixes

--- installer.sh
+++ installer.sh
@@ -1,19 +1,10 @@
 #!/usr/bin/env bash
 
-#===========================================================#
-#       _            __        ____                 __      #
-#      (_)___  _____/ /_____ _/ / /__  _____  _____/ /_     #
-#     / / __ \/ ___/ __/ __ `/ / / _ \/ ___/ / ___/ __ \    #
-#    / / / / (__  ) /_/ /_/ / / /  __/ /  _ (__  ) / / /    #
-#   /_/_/ /_/____/\__/\__,_/_/_/\___/_/  (_)____/_/ /_/     #
-#                                                           #
-#===========================================================#
-
 # Get absolute path of this script & change to that dir.
 dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $dir
 
-# Loop through all directories in the scripts path, checks if they are indeed
+# Loop through all directories in the script's path, checks if they are
 # directories, & stows them
 for directory in */; do
   if [[ -d "$directory" ]]; then
