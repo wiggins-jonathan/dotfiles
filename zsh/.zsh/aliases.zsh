5cb1783236fda7f4a86fb482ac02bc53a24676f3
Jonathan Wiggins <wiggins.jonathan@gmail.com>
2020-12-23 16:34:08 -0700

    Change ls to ll in aliases.zsh & remove ssh alias

--- zsh/.zsh/aliases.zsh
+++ zsh/.zsh/aliases.zsh
@@ -1,12 +1,11 @@
-alias ls='ls -lAhN --color=auto --group-directories-first'
+alias ll='ls -lAhN --color=auto --group-directories-first'
 alias grep='grep --color=auto'
 alias diff='diff --color=auto'
 alias ip='ip -c'
-alias ssh="TERM=termite ssh"
 alias vi=nvim
 alias tmux='tmux -f $HOME/.config/tmux/tmux.conf'
 
 # cd & ls together forever
 cdl() {
-  builtin cd "$@" && ls;
+  builtin cd "$@" && ll;
 }
