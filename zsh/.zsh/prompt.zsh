c12b4fd9333004c12785e8865a1f90d08dd1eba4
Jonathan Wiggins <wiggins.jonathan@gmail.com>
2020-12-26 20:41:02 -0700

    Change glyphs in prompt to ones that are installed by default in arch linux. Remove stagestr from vcs_info as it is not especially helpful.

--- zsh/.zsh/prompt.zsh
+++ zsh/.zsh/prompt.zsh
@@ -14,10 +14,9 @@ zstyle ':vcs_info:*' enable git # Enable vcs info for git specifically
 
 # Configure vcs_info in prompt. See GATHERING INFORMATION FROM VERSION CONTROL SYSTEMS in the zshcontrib man page
 zstyle ':vcs_info:*' check-for-changes true
-zstyle ':vcs_info:*' stagedstr '[%F{red}%f]'
-zstyle ':vcs_info:*' unstagedstr '[%F{red}%f]'
-zstyle ':vcs_info:*' formats '[%F{red}%s-%b%f] %c%u'
-zstyle ':vcs_info:*' actionformats '[%F{magenta}%s-%b%f] %c%u'
+zstyle ':vcs_info:*' formats '[%F{cyan}%s-%b%f] %u'
+zstyle ':vcs_info:*' actionformats '[%F{magenta}%s-%b%f] %u'
+zstyle ':vcs_info:*' unstagedstr '[%F{red}unstaged%f]' # This is expanded to %u in lines above
 
 # Update vcs_info when prompt is printed to screen
 precmd() {
@@ -27,4 +26,4 @@ precmd() {
 setopt PROMPT_SUBST # Set parameter expansion, command substitution, & arithmetic expansion in prompt
 
 PROMPT="%B┌──[%F{magenta}${username}%f@%F{green}${hostname}%f] [%F{blue}${pwd}%f] ${git}${newline}\
-└─%F{blue}%f%b " 
+└─%F{blue}$%f%b "
