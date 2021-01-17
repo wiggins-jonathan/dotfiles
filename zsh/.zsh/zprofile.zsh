2d5f19461acede84c8c47bed69a57d9316d739bb
Jonathan Wiggins <wiggins.jonathan@gmail.com>
2021-01-17 11:23:47 -0700

    Move instantiation of environment variables into zprofile as it is now deprecated in pam As long as the environment variables are exported before sway is called, it should work

--- zsh/.zsh/zprofile.zsh
+++ zsh/.zsh/zprofile.zsh
@@ -1,6 +1,10 @@
+# Set environment variables
+export MOZ_ENABLE_WAYLAND=1       # Required for firefox in sway
+export EDITOR="/usr/bin/nvim"
+export VISUAL=$EDITOR
+export PATH=$PATH:$HOME/.go/bin
+
 # Load sway on login if tty1
 if [ "$(tty)" = "/dev/tty1" ]; then
   exec sway
 fi
-
-export PATH=$PATH:$HOME/.go/bin
