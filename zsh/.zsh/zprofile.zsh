d71815c0eba103647dfdf5d0d658f21d69508bbe
Jonathan Wiggins <wiggins.jonathan@gmail.com>
2021-05-20 20:55:43 -0700

    Add bin to PATH & set up bin to be stowed using installer.sh

--- zsh/.zsh/zprofile.zsh
+++ zsh/.zsh/zprofile.zsh
@@ -2,7 +2,10 @@
 export MOZ_ENABLE_WAYLAND=1       # Required for firefox in sway
 export EDITOR="/usr/bin/nvim"
 export VISUAL=$EDITOR
+
+# PATH
 export PATH=$PATH:$HOME/.go/bin
+export PATH=$PATH:$HOME/bin
 
 # Load sway on login if tty1
 if [ "$(tty)" = "/dev/tty1" ]; then
