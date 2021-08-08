fcc7f83b2b2cf8f99fc5eb298f54e72cc9cfa6b2
Jonathan Wiggins <wiggins.jonathan@gmail.com>
2021-08-08 15:28:16 -0700

    Move autoloading adding of ssh key to ssh-agent from script to zsh

--- bin/bin/keyChain.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/env bash
-eval $(ssh-agent)
-ssh-add ~/.ssh/id_rsa
--- zsh/.zsh/zprofile.zsh
+++ zsh/.zsh/zprofile.zsh
@@ -7,6 +7,14 @@ export VISUAL=$EDITOR
 PATH=$PATH:$HOME/.go/bin
 PATH=$PATH:$HOME/bin
 
+# Unlock the ssh key once every hour instead of on every use
+if ! pgrep -u "$USER" ssh-agent > /dev/null; then
+    ssh-agent -t 1h > "$XDG_RUNTIME_DIR/ssh-agent.env"
+fi
+if [[ ! "$SSH_AUTH_SOCK" ]]; then
+    source "$XDG_RUNTIME_DIR/ssh-agent.env" >/dev/null
+fi
+
 # Load sway on login if tty1
 if [ "$(tty)" = "/dev/tty1" ]; then
   exec sway
