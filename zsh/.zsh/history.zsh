8bce819b559c9aaa8d43aa68d39f3a915782e2b7
Jonathan Wiggins <wiggins.jonathan@gmail.com>
2021-12-21 11:00:27 -0700

    Don't save commands in history that have a leading space character

--- zsh/.zsh/history.zsh
+++ zsh/.zsh/history.zsh
@@ -5,3 +5,4 @@ export HISTFILE=$HOME/.zsh/zhistory # save to history file
 setopt INC_APPEND_HISTORY           # append to this history file
 setopt EXTENDED_HISTORY             # add timestamp for each history entry
 setopt HIST_IGNORE_DUPS             # avoid duplicate entries
+setopt HIST_IGNORE_SPACE            # remove commands that contain leading space
