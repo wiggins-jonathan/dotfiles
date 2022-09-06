69eeb6a1aac5aa7e3757d22c483197a7399cad12
Jonathan Wiggins <wiggins.jonathan@gmail.com>
2022-09-06 12:07:40 -0700

    remove tmux alias
    tmux 3.1 added support for autoloading a file in ~/.config/tmux/tmux.conf
    tmux 3.2 added support for autoloading $XDG_CONFIG_HOME/tmux/tmux.conf
    so this alias is no longer needed

--- zsh/.zsh/aliases.zsh
+++ zsh/.zsh/aliases.zsh
@@ -3,7 +3,6 @@ alias grep='grep --color=auto'
 alias diff='diff --color=auto'
 alias ip='ip -c'
 alias vi=nvim
-alias tmux='tmux -f $HOME/.config/tmux/tmux.conf'
 alias venv='source .venv/bin/activate'
 alias tf='terraform'
 alias ap='ansible-playbook'
