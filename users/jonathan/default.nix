143fe168e21e8d50fcd50bf58f706998cc773e49
Jonathan Wiggins <jonathan@wiggins.tech>
2025-01-05 23:19:54 -0700

    update user public ssh key

--- users/jonathan/default.nix
+++ users/jonathan/default.nix
@@ -5,7 +5,7 @@
     isNormalUser = true;
     extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
     openssh.authorizedKeys.keys = [
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBSlnCzEq8KrEnCV0lisLzlO87/79lS5Ot4IjV4Oa2G3 deck@steamdeck"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFRxSgJBmsOFmOIdrt1qVCZSqeLMs7NzIHsFFZ0XqM0k deck@steamdeck"
     ];
     initialHashedPassword = "$y$j9T$RZ3Xvd417pxVsAOLbkKb91$WK10KAGV1B5/KRm6AewemibV/8rcZ3V7wGpUo1kvaK4";
     shell = pkgs.zsh;
