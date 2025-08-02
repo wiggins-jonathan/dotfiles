eddb60bc3a86cde4f3975f81e4db364771897854
Jonathan Wiggins <jonathan@wiggins.tech>
2025-08-02 12:39:06 -0700

    update to nix 25.05

--- flake.nix
+++ flake.nix
@@ -2,9 +2,9 @@
   description = "system config";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11-small";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05-small";
 
-    home-manager.url = "github:nix-community/home-manager/release-24.11";
+    home-manager.url = "github:nix-community/home-manager/release-25.05";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
